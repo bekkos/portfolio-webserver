4e28f29d531fcafdc32215e1e638d6ac5936d684
bekkosm <bekkosm@gmail.com>
2022-03-29 20:54:45 +0200

    a

---
+++ gps/js/logic.js
@@ -0,0 +1,9 @@
+let geo = navigator.geolocation;
+
+const success = (data) => {
+    console.log(data);
+    window.open("https://www.google.com/maps/@" + data['coords']['latitude'] + "," + data['coords']['longitude'] + ",23z", '_blank')
+}
+
+geo.getCurrentPosition(success);
+document.getElementById("output").innerText(position);
