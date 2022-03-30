f33d99741e5c092a2fb8d7cadf5719b83a9aab6f
bekkosm <bekkosm@gmail.com>
2022-03-30 14:16:12 +0200

    a

--- game/js/logic.js
+++ game/js/logic.js
@@ -1,9 +1,10 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let _STATE = false;
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-const _cellSize = 16;
+const _cellSize = 8;
 const _COLUMNS = Math.floor(window.innerWidth / _cellSize);
 const _ROWS = Math.floor(window.innerHeight / _cellSize);
 
@@ -24,13 +25,20 @@ const render = () => {
             } else {
                 ctx.fillStyle = "#000";
             }
-            ctx.fillRect(16 * i, 16 * j, 16, 16);
+            ctx.fillRect(_cellSize * i, _cellSize * j, _cellSize, _cellSize);
         }
     }
 
 
 
-    ctx.strokeStyle = '#cf3838';
+    if(_STATE) {
+        ctx.strokeStyle = '#12802f';
+        
+    } else {
+        ctx.strokeStyle = '#cf3838';
+    }
+
+
     for(let y = 0; y < _COLUMNS; y++) {
         ctx.beginPath();
         ctx.moveTo(y * _cellSize, 0);
@@ -49,21 +57,25 @@ const render = () => {
 
 const initiateBoard = () => {
     
-    for(let i = 0; i < _COLUMNS; i++) lastGen.push([]);
-    for(let i = 0; i < _ROWS; i++) lastGen.push([]);
+    for(let i = 0; i < _COLUMNS; i++) {
+        lastGen.push([]);
+    }
+    
     for(let i = 0; i < lastGen.length; i++) {
         for(let j = 0; j < _ROWS; j++) {
-            if(Math.floor(Math.random() * 99) > 100) {
+            if(Math.floor(Math.random() * 99) > 99) {
                 lastGen[i][j] = true;
             } else {
                 lastGen[i][j] = false;
             }
-        
         }
     }
+
+    for (let i = 0; i < lastGen.length; i++) {
+        currentGen[i] = lastGen[i].slice();
+    }
+    console.log(currentGen);
     
-    
-    currentGen = lastGen;
 
     render();
 }
@@ -120,20 +132,49 @@ const updateGame = () => {
                 continue;
             }
             
+            if(neighbours > 1) {
+                console.log(i + " | " + j + " | " + neighbours);
+            }
 
             currentGen[i][j] = false;
         }
     }
 
-    lastGen = currentGen;
+
+    for (let i = 0; i < currentGen.length; i++) {
+        lastGen[i] = currentGen[i].slice();
+    }
+
+    // for(let i = 0; i < lastGen.length; i++) {
+    //     for(let j = 0; j < lastGen[i].length; j++) {
+    //         currentGen[i][j] = lastGen[i][j]
+
+    //     }
+    // }
+    
 }
 
 window.addEventListener('click', event => {
     console.log(Math.floor(event.x / _cellSize) + " | " + Math.floor(event.y / _cellSize));
     let x = Math.floor(event.x / _cellSize);
     let y = Math.floor(event.y / _cellSize);
+    lastGen[x][y] = !lastGen[x][y]
     currentGen[x][y] = !currentGen[x][y]
     render();
+});
+
+window.addEventListener('keydown', event => {
+    if(event.keyCode == 32) {
+        if(_STATE == false) {
+            start();
+        } else {
+            pause();
+        }
+        _STATE = !_STATE;
+        render();
+        console.log(_STATE);
+    }
+    
 });
 
 
@@ -150,7 +191,7 @@ const start = () => {
     setInterval(() => {
         updateGame();
         render();
-    }, 500);
+    }, 50);
 }
 
 
